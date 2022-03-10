f7c63e76b006eacc93aa366e9041dda95f2137b1
Radoncode <relbouni69@gmail.com>
2022-03-10 01:49:16 +0000

    using collect and map method

--- routes/web.php
+++ routes/web.php
@@ -18,19 +18,20 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {   
     $files = File::files(resource_path("posts/"));
-    $posts = [];
+    
+    $posts = collect($files)
+        ->map(function($file){
+            $document = YamlFrontMatter::parseFile(($file));
+    
+            return new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            );
+        }, $files);
 
-    foreach($files as $file) {
-        $document = YamlFrontMatter::parseFile(($file));
-
-        $posts[] = new Post(
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body(),
-            $document->slug
-        );
-    }
     
     return view('posts', [
         'posts' => $posts

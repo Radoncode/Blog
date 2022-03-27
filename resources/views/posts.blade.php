0991577d981bc3a0f049bcfddd06e65fda75fceb
Radoncode <relbouni69@gmail.com>
2022-03-27 16:37:54 +0100

    understand how blade works

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,16 +4,22 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <?php foreach($posts as $post): ?>
-    <article>
+
+    @unless
+
+    @endunless
+
+    @foreach ($posts as $post)
+
+    <article class="{{ $loop->even ? 'foobar' : '' }}">
        <h1>
-           <a href="/posts/<?= $post->slug; ?>">
-                <?= $post->title; ?>
+           <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
            </a>
         </h1>
        <div>
-           <?= $post->excerpt; ?>
+           {{ $post->excerpt }}
        </div>
     </article>
-    <?php endforeach; ?>
+    @endforeach
 </body>
